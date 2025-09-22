d64c20ea97fd8efdab8c8dffdc223c5923f3760b
Vivek Kumar <vh118854@gmail.com>
2025-09-23 01:05:42 +0530

    new Page added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React from "react";
 import logo from "../assets/Johar_Tourism_01.png";
+import { Facebook, Instagram, Twitter } from "lucide-react";
 
 function Footer() {
   return (
-    <div>
-      <footer className='grid grid-cols-1 md:grid-cols-3 p-4'>
-        <div>
-            <div className='flex items-center'>
-                <img src={logo} alt="" className='h-12 w-14' />
-                <h1 className='font-bold text-xl'>Johar <span className='text-blue-500'>Tourism</span></h1>
-            </div>
+    <div className="w-full bg-black py-16 px-10 grid md:grid-cols-2 gap-8 text-gray-300 bottom-0">
+      <div>
+        <h3 className="text-2xl font-bold text-[#41A4FF]">Johar Tourism</h3>
+        <p className="py-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A odio non
+          nemo veniam, natus accusantium. Praesentium, doloribus mollitia
+          dignissimos similique optio
+        </p>
+        <div className="flex gap-10 justify-start my-6 md:w-[75%] cursor-pointer">
+          <Facebook size={30} />
+          <Twitter size={30} />
+          <Instagram size={30} />
         </div>
+      </div>
+      <div className="flex md:justify-around justify-start mt-8">
         <div>
-            <ul className='text-md cursor-pointer'>
-                <li>Home</li>
-                <li>Privacy Policy</li>
-                <li>Contact us</li>
-            </ul>
+          <h6 className="font-bold text-[#41a3ff] text-xl">Reservations</h6>
+          <ul className="mt-2 font-light cursor-pointer">
+            <li className="py-2 text-sm">Hotels</li>
+            <li className="py-2 text-sm">Tour Packages</li>
+            <li className="py-2 text-sm">Vehicles</li>
+            <li className="py-2 text-sm">Restaurants</li>
+            <li className="py-2 text-sm">Events</li>
+          </ul>
         </div>
-        <div>Right</div>
-      </footer>
+        <div className="ml-[8rem]">
+          <h6 className="font-bold text-[#41A4FF] text-xl">Support</h6>
+          <ul className="mt-2 font-light cursor-pointer">
+            <li className="py-2 text-sm">Contact us</li>
+            <li className="py-2 text-sm">About us</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/pages/Reservation.jsx
+++ frontend/src/pages/Reservation.jsx
@@ -5,12 +5,7 @@ function Reservation() {
   return (
     <div>
       <Navbar />
-      <div className="flex gap-4">
-        <h1 className="text-2xl font-bold ">Railway Tickets</h1>
-        <button className="px-4 py-2 text-white bg-blue-400 cursor-pointer hover:bg-blue-900">
-          Click Here
-        </button>
-      </div>
+      
     </div>
   );
 }
